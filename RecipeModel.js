6f68e791126cd28574bae170645eeba93b0d24dc
LVL <loithui162@gmail.com>
2020-08-31 17:26:00 +0700

    1

---
+++ RecipeModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+//Recipe Model
+const recipeSchema = mongoose.Schema({
+    "name": {
+        "type": "String"
+    },
+    "description": {
+        "type": "String"
+    },
+    "ingredients": {
+        "type": "Object"
+    },
+    "cooking_steps": {
+        "type": [
+            "String"
+        ]
+    },
+    "image": {
+        "type": "String"
+    },
+    "ration": {
+        "type": "String"
+    },
+    "likes": {
+        "type": "Number"
+    }
+})
+const Recipe = mongoose.model('Recipe', recipeSchema)
+
+module.exports = Recipe
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const PORT = 8080
+const Recipe = require('./RecipeModel');
+const { Recoverable } = require('repl');
+const db = mongoose.connection;
+//Connect to cloud db
+let db_conString = "mongodb+srv://lvl162:1622000@cluster0.gabg8.gcp.mongodb.net/CookyCooky?retryWrites=true&w=majority"
+mongoose.connect(db_conString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => console.log("Sucessfully connected!!"))
+
+
+
+app.use('/', (req, res) => {
+    //res.json({ "mess": "Hello Would!" })
+    Recipe.findById('5f4cbf6f2b491f0fac503105', (err, recipe) => {
+        if (err) { return res.json({ err }) }
+        res.json({
+            name: recipe.name,
+            nguyen_lieu: recipe.ingredients
+        })
+    })
+})
+
+app.listen(PORT, () => { console.log("Server started on http://localhost:" + PORT) })
+
+
+
+
+module.exports = app;
